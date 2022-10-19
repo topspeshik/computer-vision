65385525b040c107e479a30b5cf965655346d3b4
Daniel <speshik222@gmail.com>
2022-10-19 16:56:54 +0800

    add task4

---
+++ task4_count/main.py
@@ -0,0 +1,62 @@
+import numpy as np
+from skimage.measure import label
+from scipy.ndimage.morphology import binary_opening
+
+masks = np.array([
+    np.array([
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1]
+    ]),
+    np.array([
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1, 1]
+    ]),
+    np.array([
+        [1, 1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1]
+    ]),
+    np.array([
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
+    ]),
+    np.array([
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 0, 0],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
+    ]),
+    np.array([
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [0, 0, 1, 1],
+        [0, 0, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1]
+    ])
+])
+
+img = np.load('ps.npy')
+imgSum = label(img).max()
+print(f"Total - {imgSum}")
+count = 0
+for mask in masks:
+    maskedImg = binary_opening(img, mask)
+    labelImg = label(maskedImg)
+    img -= maskedImg
+    print(f"For \n {mask} \n total = {labelImg.max()}")
+
+
+
+
